a98869bbfe8b5bcdf50866a3d4acc6bfa3b56191
Sammirah <103369979+Sammirah@users.noreply.github.com>
2023-12-08 18:14:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload() {
+    img = loadImage('violin.jpeg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    fill("#FF0000")
+    text("violino", 45, 75);
+    noFill();
+    stroke("#FF0000");
+}
